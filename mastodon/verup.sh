f1c540b204579ef45e95c2597d4dff120b2d33f9
stormcorgi <70886974+stormcorgi@users.noreply.github.com>
2023-09-22 00:21:51 +0900

    script apply changes onto k3s cluster

--- mastodon/verup.sh
+++ mastodon/verup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sed -i "" -e "s/image: .*mastodon:.*/image: ghcr.io\/mastodon\/mastodon:v${1}/g" *.yaml
+find ./*.yaml -print0 | xargs -0 -I {} sh -c "kubectl apply -f {}"
+
+# test
+# find ./*.yaml -print0 | xargs -0 -I {} sh -c "echo {}"

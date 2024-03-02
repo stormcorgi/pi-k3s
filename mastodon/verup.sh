a0212f3777dcd5fd3a44d267799e4424524173b4
stormcorgi <70886974+stormcorgi@users.noreply.github.com>
2024-03-02 11:04:46 +0900

    improve handling

--- mastodon/verup.sh
+++ mastodon/verup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "$0")" && pwd -P)"
-echo $DIR
-cd $DIR || exit
+echo "${DIR}"
+cd "${DIR}" || exit
 echo "verup start! target version: ${1}"
 
 kube=$(which kubectl)
 if [ -z "$kube" ]; then
     echo "kubectl not found. set /snap/bin/kubectl"
-    $kube="/snap/bin/kubectl"
+    kube="/snap/bin/kubectl"
 fi
-echo $kube
+echo "${kube}"
 
-sed -i "" -e "s/image: .*mastodon:.*/image: ghcr.io\/mastodon\/mastodon:${1}/g" *.yaml
-find ./*.yaml -print0 | xargs -0 -I {} $kube apply -f {}
+sed -i "" -e "s/image: \(.*mastodon\):.*/image: \1:${1}/g" ./*.yaml
+find . -name "*.yaml" -not -path './init/*' -print0 | xargs -0 -I {} $kube apply -f {}
 
-# test
-# find ./*.yaml -print0 | xargs -0 -I {} sh -c "echo {}"
+# debug
+# find . -name "*.yaml" -not -path './init/*' -print0 | xargs -0 -I {} echo {}
